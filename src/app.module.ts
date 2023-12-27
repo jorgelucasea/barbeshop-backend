c9534120664df0153a3f4091f483712acddb1e0b
Jorge Lucas <jorgelucasea@gmail.com>
2023-12-27 02:29:00 -0300

    (chore) - configs para graphql e postgree + docker postgree

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientesModule } from './clientes/clientes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'your_user',
+    password: 'your_password',
+    database: 'barbershop_schema',
+    entities: [__dirname + '***/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }),
+  ClientesModule,
+],
 })
 export class AppModule {}
